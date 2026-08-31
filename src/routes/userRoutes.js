5b6f79bcea3e20d4873c3e08f747b9b9de15532e
Yana Khariebova <harebova.yana@gmail.com>
2026-08-31 13:32:20 +0200

    Add user routes for user management functionality

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,22 @@
+// importiere die benötigten Module und Controller
+import express from "express";
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
+
+// Erstelle einen Router für die Benutzerverwaltung
+const router = express.Router();
+// Definiere die Routen für die Benutzerverwaltung
+router.get("/", getUsers);
+router.post("/", createUser);
+router.get("/:id", getUserById);
+router.patch("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+// Exportiere den Router, damit er in anderen Teilen der Anwendung verwendet werden kann
+export default router;
+
